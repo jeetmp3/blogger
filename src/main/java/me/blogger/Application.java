c210bfb2742870d1cc26537473d87c24c46c73e4
Jitendra Singh Bisht <jitendra.singh@tothenew.com>
2016-06-26 18:43:45 +0530

    WAR file support added

--- src/main/java/me/blogger/Application.java
+++ src/main/java/me/blogger/Application.java
@@ -2,12 +2,19 @@ package me.blogger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * @author Jitendra Singh.
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
